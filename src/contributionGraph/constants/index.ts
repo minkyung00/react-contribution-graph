6cf0b773bea7b82e7e6a5b1e896c924a2ee4832b
minkyung00 <mksin00@naver.com>
2023-01-25 15:35:56 +0900

    chore: Add constants - width, height, color

---
+++ src/contributionGraph/constants/index.ts
@@ -0,0 +1,14 @@
+export const SIZE = 20;
+export const WIDTH = 1100;
+export const HEIGHT = 180;
+export const WEEK_DAYS = 7;
+export const ACTIVE_COLOR = [
+  "#e5e7eb",
+  "#a5b4fc",
+  "#818cf8",
+  "#6366f1",
+  "#4f46e5",
+  "#4338ca",
+  "#3730a3",
+  "#312e81",
+];
