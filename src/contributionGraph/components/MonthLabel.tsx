e1593842b3025d38e8dcbd188efcc647838f7a59
minkyung00 <mksin00@naver.com>
2023-01-27 18:06:18 +0900

    fix: MonthLabel's x position

--- src/contributionGraph/components/MonthLabel.tsx
+++ src/contributionGraph/components/MonthLabel.tsx
@@ -7,32 +7,18 @@ type MonthLabelProps = {
 
 export function MonthLabel({ months }: MonthLabelProps) {
   const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth(); // 0부터 시작
-  const currentDate = new Date().getDate(); // 0부터 시작
+  const currentMonth = new Date().getMonth();
+
   return (
     <>
       {Array.from({ length: months }).map((_, monthIndex) => {
         const month =
           new Date(currentYear, currentMonth - monthIndex).getMonth() + 1;
-        const weekCounts = Math.floor(
-          (+new Date(
-            currentYear,
-            currentMonth - monthIndex,
-            monthIndex === 0 ? currentDate : 1
-          ) -
-            +new Date(
-              currentYear,
-              monthIndex === 0 ? currentMonth : currentMonth - monthIndex - 1
-            )) /
-            86400000 /
-            7
-        );
-        console.log(WIDTH - SIZE * (monthIndex + 1) * weekCounts);
+        const diffDays =
+          (+new Date() - +new Date(currentYear, currentMonth - monthIndex, 1)) /
+          86400000;
         return (
-          <Label
-            x={WIDTH - SIZE * (monthIndex + 1) * weekCounts + SIZE}
-            y={SIZE * (WEEK_DAYS + 1)}
-          >
+          <Label x={WIDTH - SIZE * (diffDays / 7)} y={SIZE * (WEEK_DAYS + 1)}>
             {month}월
           </Label>
         );
